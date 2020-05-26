70eddb1709ec8023a62f9094a228dd9ffe99951f
btvoidx <btvoidx@gmail.com>
2020-05-26 21:29:03 +0400

    Now we can create images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from PIL import Image, ImageDraw
 
 def load_scenes(file:str = "input.json"):
 	# Open file, read it, load into scenes list
@@ -8,8 +9,16 @@ def load_scenes(file:str = "input.json"):
 	return scenes
 
 def main(scenes:list):
+	scene_number = 0
 	for scene in scenes:
-		print(scene)
+		scene_number += 1
+		# Create new image
+		img = Image.new('RGB', tuple(scene["dimensions"]), color = tuple(scene["colors"]["background"]))
+		draw = ImageDraw.Draw(img)
+		draw.text((10,10), scene["text"], fill=tuple(scene["colors"]["text"]))
+
+		# Save the image
+		img.save(f"scene{scene_number}.png")
 
 if __name__ == '__main__':
 	scenes = load_scenes()
